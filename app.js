2fe4672d192be1c7ef6fdf99241bef2c4634bc85
MaryFerH <fer.hs19@gmail.com>
2020-04-06 11:41:10 -0500

    Se incio la aplicación

---
+++ app.js
@@ -0,0 +1,13 @@
+const express =require('express'),
+ createError= require('http-errors'),
+ routes = require('./routes/index'),
+ app = express()
+ //app.get('/', (req, res) => res.send('Hello World!'))
+app
+ .use(routes)
+ .use((req,res,next)=>next(createError(404)))
+ .use((error,req,res,next)=>{
+  res.status(error.static||500)
+  res.render('error: ')
+ })
+ .listen(3000,()=>console.log("Se inicio en el puerto 3000"))
